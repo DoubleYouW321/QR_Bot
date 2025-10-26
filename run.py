b6d98e9bbcd5c31284a5af13e294930bf54b2a86
DoubleYouW321 <grigorijbatisev739@gmail.com>
2025-10-26 21:53:48 +0300

    host

--- run.py
+++ run.py
@@ -2,8 +2,13 @@ import asyncio
 
 import logging as log
 from aiogram import Bot, Dispatcher
-from config import TOKEN
 from app.handlers import router
+import os
+
+try:
+    from config import TOKEN
+except ImportError:
+    TOKEN = os.getenv('BOT_TOKEN')
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
